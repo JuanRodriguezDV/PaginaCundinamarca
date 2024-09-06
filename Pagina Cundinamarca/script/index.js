bd4edab2168be70d21d0ccc6311192bd8598c252
Juan Sebastián Rodriguez <rolo.velosa@gmail.com>
2024-09-06 15:39:40 -0500

    final

--- Pagina Cundinamarca/script/index.js
+++ Pagina Cundinamarca/script/index.js
@@ -1,70 +1,84 @@
 function validarNombre(event) {
+    event.preventDefault();
     var nombre = document.getElementById("nombre").value
     var celular = document.getElementById("celular").value
     var correo = document.getElementById("correo").value
     var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     var correoOk = regex.test(correo);
-    if (!correoOk) {
-        alert("correo negado, verifica tu correo")
-    }
-
-   
-  
-
     var valorSi = document.getElementById("si")
     var valorNo = document.getElementById("no")
-
     var region = document.getElementById("region").value
     var tipoCc = document.getElementById("tipoCc")
     var tipoCll = document.getElementById("tipoCll")
     var tipoCw = document.getElementById("tipoCw")
-    var tipoDeContacto = []
+    var tipoContacto = []
+    if (!correoOk) {
+        alert("correo negado, verifica tu correo")
+        return;
+    }
 
-    if (tipoCc.checked == true) {
-        tipoDeContacto.push(tipoCc.value)
+    if (tipoCc.checked) {
+        tipoContacto.push(tipoCc.value)
     }
-    if (tipoCll.checked == true){
-        tipoDeContacto.push(tipoCll.value)
+    if (tipoCll.checked){
+        tipoContacto.push(tipoCll.value)
     }
-    if (tipoCw.checked == true){
-        tipoDeContacto.push(tipoCw.value)
+    if (tipoCw.checked){
+        tipoContacto.push(tipoCw.value)
     }
 
     localStorage.setItem('nombre',nombre)
-    localStorage.setItem('numero',celular)
-    localStorage.setItem('email',correo)
+    localStorage.setItem('celular',celular)
+    localStorage.setItem('correo',correo)
     localStorage.setItem('region',region)
-    localStorage.setItem('masInfo',valorSi)
-    localStorage.setItem('masInfo',valorNo)
-    localStorage.setItem('tipoDeContactp',tipoDeContacto)
+    localStorage.setItem('masInfo',valorSi.checked ? 'si': valorNo.checked ? 'no' : 'N/A')
+    localStorage.setItem('tipoContacto',JSON.stringify(tipoContacto))
 
-    location.href = "../pages/pageDatos.html"
+    location.href = "/Pagina Cundinamarca/pages/pageDatos.html"
    
 }
 
 function Datos() {
+    nombre      = localStorage.getItem('nombre')
+    celular     = localStorage.getItem('celular')
+    email       = localStorage.getItem('correo')
+    region      = localStorage.getItem('region')
+    masInfo     = localStorage.getItem('masInfo')
+    tipoContacto= JSON.parse(localStorage.getItem('tipoContacto'))
+    
+    document.getElementById("nombreOk").value= nombre
+    document.getElementById("celularOk").value=celular
+    document.getElementById("correoOk").value=email
+    document.getElementById("regionOk").value=region
+    if(masInfo == "si"){
+        document.getElementById("masInfoOk").value="Si";
+    }else if (masInfo == "no"){
+        document.getElementById("masInfoOk").value="No";
+    }
 
-    nombre=localStorage.getItem('nombre')
-    console.log(nombre)
-    document.getElementById("nombreOk").value=nombre
+    document.getElementById("tipoCOk").value = (Array.isArray(tipoContacto) && tipoContacto.length > 0)
+    ? tipoContacto.join(", ")
+    : 'N/A';
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-    function masInfo() {
-        valorSi = document.getElementById("si")
-        valorNo = document.getElementById("no")
-       
-        var divTipoC = document.getElementById("tipoContacto")
-        
-        if (valorSi.checked) {
-            divTipoC.hidden = false
-        }
-        if (valorNo.checked) {
-            divTipoC.hidden = true
-        }
-    }
-    document.getElementById("si").onclick = masInfo
-    document.getElementById("no").onclick = masInfo
+    valorSi = document.getElementById("si")
+    valorNo = document.getElementById("no")
+    if(valorSi && valorNo){
+        function masInfo() {
+            var divTipoC = document.getElementById("tipoContacto")
+            
+            if (valorSi.checked) {
+                divTipoC.hidden = false
+                
+            }
+            if (valorNo.checked) {
+                divTipoC.hidden = true
+            }
+    }}
+    valorSi.addEventListener("change", masInfo);
+    valorNo.addEventListener("change", masInfo);
+    masInfo()
 
 })
 
